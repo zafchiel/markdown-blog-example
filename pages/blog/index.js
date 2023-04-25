f8e4137e9251d998c1dddd6e7953c07b8a51b524
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-04-25 17:24:31 +0200

    Blog Page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,44 @@
+import fs from "fs"
+import path from "path"
+import Link from "next/link"
+import matter from "gray-matter"
 import Layout from "@/components/Layout"
+import Post from "@/components/Post"
+import { sortByDate } from "@/utils/index"
 
-export default function BlogPage() {
-  return <Layout title="Blog">BlogPage</Layout>
+export default function BlogPage({ posts }) {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"))
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "")
+
+    const markdownWithMeta = fs.readFileSync(path.join("posts", filename))
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate),
+    },
+  }
 }
