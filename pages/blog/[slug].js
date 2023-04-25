9371109655c513134ec5f3d9ed4429de20cf7eed
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-04-25 18:27:16 +0200

    Single Blog Post Page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,22 +1,57 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
+import { marked } from "marked"
+import Image from "next/image"
+import Link from "next/link"
+import Layout from "@/components/Layout"
+import CategoryLabel from "@/components/CategoryLabel"
 
 export default function PostPage({
   frontmatter: {
     title,
     category,
-    data,
     cover_image,
     author,
+    author_image,
     date,
-    read_time,
     excerpt,
     tags,
   },
   content,
+  slug,
 }) {
-  return <div>{title}</div>
+  console.log(author)
+  return (
+    <Layout title={title}>
+      <Link href="/blog">Go Back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+        <img src={cover_image} alt="" className="w-full rounded" />
+
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <Image
+              width={24}
+              height={24}
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
@@ -51,6 +86,7 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       frontmatter,
       content,
+      slug,
     },
   }
 }
