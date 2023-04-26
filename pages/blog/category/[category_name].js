6b2f37f3960c54adbdba62313b3d5616da723efe
Zafchiel <114311830+zafchiel@users.noreply.github.com>
2023-04-26 23:27:55 +0200

    Categories menu

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -2,20 +2,29 @@ import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
 import Layout from "@/components/Layout"
+import CategoryList from "@/components/CategoryList"
 import Post from "@/components/Post"
 import { getPosts } from "@/lib/posts"
 
-export default function CategoryPage({ posts, categoryName }) {
+export default function CategoryPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">
-        {categoryName.toUpperCase()}
-      </h1>
+      <div className="flex justify-between py-4">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">
+            Posts in {categoryName.toUpperCase()}
+          </h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
@@ -49,10 +58,15 @@ export async function getStaticProps({ params: { category_name } }) {
     (post) => post.frontmatter.category.toLowerCase() === category_name
   )
 
+  // Get categories for siedbar
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
+
   return {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   }
 }
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -3,21 +3,29 @@ import path from "path"
 import Layout from "@/components/Layout"
 import Post from "@/components/Post"
 import Pagination from "@/components/Pagination"
+import CategoryList from "@/components/CategoryList"
 import { POSTS_PER_PAGE } from "@/config"
 import { getPosts } from "@/lib/posts"
 
-export default function BlogPage({ posts, currentPage, numPages }) {
+export default function BlogPage({ posts, currentPage, numPages, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+      <div className="flex justify-between py-4">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
-
-      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -46,6 +54,11 @@ export async function getStaticProps({ params }) {
 
   const posts = getPosts()
 
+  // Get categories for siedbar
+  const categories = posts.map((post) => post.frontmatter.category)
+  const uniqueCategories = [...new Set(categories)]
+
+  // Pagination setup
   const numPages = Math.ceil(posts.length / POSTS_PER_PAGE)
   const pageIndex = currentPage - 1
   const orderedPosts = posts.slice(
@@ -58,6 +71,7 @@ export async function getStaticProps({ params }) {
       posts: orderedPosts,
       currentPage,
       numPages,
+      categories: uniqueCategories,
     },
   }
 }
