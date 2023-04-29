adbad0b0122e566fe6c114d1ded0ae371894e720
Zafchiel <bialakropla127.9@o2.pl>
2023-04-29 15:36:49 +0200

    Code cleanup / uninstall husky

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,46 +3,31 @@ import matter from "gray-matter"
 import path from "path"
 
 export default function handler(req, res) {
+  let files
   let posts
 
   if (process.env.NODE_ENV === "production") {
     // Fetch from cache
-    const files = fs.readdirSync(path.join(process.cwd(), "posts"))
-
-    posts = files.map((filename) => {
-      const slug = filename.replace(".md", "")
-
-      const markdownWithMeta = fs.readFileSync(
-        path.join("posts", filename),
-        "utf-8"
-      )
-
-      const { data: frontmatter } = matter(markdownWithMeta)
-
-      return {
-        slug,
-        frontmatter,
-      }
-    })
+    files = fs.readdirSync(path.join(process.cwd(), "posts"))
   } else {
-    const files = fs.readdirSync(path.join("posts"))
-
-    posts = files.map((filename) => {
-      const slug = filename.replace(".md", "")
-
-      const markdownWithMeta = fs.readFileSync(
-        path.join("posts", filename),
-        "utf-8"
-      )
-
-      const { data: frontmatter } = matter(markdownWithMeta)
-
-      return {
-        slug,
-        frontmatter,
-      }
-    })
+    files = fs.readdirSync(path.join("posts"))
   }
+
+  posts = files.map((filename) => {
+    const slug = filename.replace(".md", "")
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    )
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
 
   const results = posts.filter(
     (post) =>
